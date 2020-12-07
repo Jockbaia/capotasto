4a7e97375720e2056b5303942b4716dea27f0be7
Antonio Macaluso <jockbaia@gmail.com>
2020-12-07 15:15:35 +0100

    header.php fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- * @package Picopod
+ * @package Capotasto
  */
 get_header();
 ?>
