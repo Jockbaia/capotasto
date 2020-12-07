30bb1bc0289b4598323f1a4ef4578616f12f5d3a
Antonio Macaluso <jockbaia@gmail.com>
2020-12-07 16:17:51 +0100

    minor fixes

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
  * E.g., it puts together the home page when no home.php file exists.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- * @package Capotasto
+ * @package Picopod
  */
 get_header();
 ?>
