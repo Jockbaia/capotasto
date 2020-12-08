cc21b7f4497fcd8aa3df2e62c9537d56191282fa
Piero Macaluso <piero.macaluso@linksfoundation.com>
2020-12-08 18:38:55 +0100

    Fix Theme Utility

--- js/themeUtility.js
+++ js/themeUtility.js
@@ -1,5 +1,5 @@
 function inputFocus() {
-    if (document.getElementById('header-search').style.display == 'none') {
+    if (document.getElementById('header-search').style.display === "none") {
         document.getElementById('input-search').focus();
     } else {
         document.getElementById('input-search').blur();
